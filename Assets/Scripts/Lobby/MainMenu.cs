e3eaa7c07889180132af8b248c3f4e9f87e323d0
Noé <noepasquer0@gmail.com>
2024-01-23 15:14:12 +0100

    first scripts

---
+++ Assets/Scripts/Lobby/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class MainMenu: MonoBehaviour
+
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+        landingPagePanel.SetActive(false);
+    }
+    /*
+    public void JoinLobby()
+    {
+        networkManager.StartClient();
+        landingPagePanel.SetActive(false);
+    }
+    */
+    
+    
+}
